87b48731246065c13fe39eb0c90db3e6d8c6b184
Yash-Chopra20BCS6402 <99278322+Yash-Chopra20BCS6402@users.noreply.github.com>
2024-04-15 15:33:52 +0530

    Changed

--- src/main/scala/PairSum.scala
+++ src/main/scala/PairSum.scala
@@ -5,29 +5,18 @@ object PairSum extends App {
   def pairs(nums: Set[Int], target: Int): List[Set[Int]] = {
     val result = ListBuffer[Set[Int]]()
 
-    def helper(start: Int, remaining: Int, path: Set[Int]): Unit = {
-      if (remaining == 0) {
-        result += path
-        return
-      }
-      if (remaining < 0 || start >= nums.size) {
-        return
-      }
-      val num = nums.toList(start)
-      if (num <= remaining && !path.contains(num)) {
-        helper(start + 1, remaining, path)
-        helper(start + 1, remaining - num, path + num)
-      } else {
-        helper(start + 1, remaining, path)
+    for (i <- nums) {
+      val complement = target - i
+      if (nums.contains(complement) && complement != i) {
+        result += Set(i, complement)
       }
     }
 
-    helper(0, target, Set())
     result.toList
   }
 
   val nums = Set(1, 2, 3, 4, 5)
   val target = 8
-  val combinations = pairs(nums, target).filter(_.sum == target)
+  val combinations = pairs(nums, target)
   println(combinations)
 }
